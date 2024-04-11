291d9dea8d8db9a4303ed50a3912dca8f365ee55
Anreet Kaur <anreetka@sheridancollege.ca>
2024-04-11 15:42:37 -0400

    added implementation settings and fetching user token

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
+import { useEffect } from 'react';
 import './App.css';
 
-
-const CLIENT_ID = process.env.MY_CLIENT_ID;
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 
 function App() {
+
+  useEffect(()=>{
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParams = urlParams.get("code");
+    console.log(codeParams);
+  }, [])
 
   function loginWithGithub(){
     window.location.assign("https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID);
   }
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' className='github-img' alt='github-logo' /><br/>
         <p className='begin-p'>To continue, please sign in with your github below:</p>
-        <button className='git-signup'>Sign in with Github (SSO)</button>
+        <button className='git-signup'
+        onClick={loginWithGithub}
+        >Sign in with Github (SSO)</button>
       </header>
     </div>
   );
