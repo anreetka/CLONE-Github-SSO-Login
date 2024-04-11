79d0f528d934a3cbc83be888c784bd1b4a5ace40
Anreet Kaur <anreetka@sheridancollege.ca>
2024-04-11 16:17:04 -0400

    added routes for getting github user data and token access

--- server/server.js
+++ server/server.js
@@ -15,6 +15,39 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+//get token access from the frontend
+app.get('/getTokenAccess', async function(req, res){
+    const accessToken = req.query.code;
+    const params  = '?client_id=' + CLIENT_ID + "&client_secret" + CLIENT_SECRET + "&code=" + accessToken;
+    const response = await fetch("https://github.com/login/oauth/access_token" + params, {
+        method: 'POST',
+        headers:{
+            "Accept": "application/json"
+        }
+    }).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        res.json(data);
+    })
+});
+
+
+//get user data
+app.get('/getUserData', async function(req, res){
+    req.get("Authorization"); //Bearer ACCESSTOKEN
+
+    const response = await fetch("https://api.github.com/user", {
+        method: "GET",
+        header:{
+            "Authorization" : req.get("Authorization")
+        }
+    }).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        res.json(data);
+    })
+})
+
 const port = 4000;
 
 app.listen(port, function(){
